3b4e0bd0396ede3d27f3bad1ac6d0e5c9685f7f5
DenysBosiak <b.denis92@gmail.com>
2024-03-19 11:51:14 +0200

    update Exercise-2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,31 @@
 import requests
-import pandas
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 def main():
     # your code here
-    pass
+    URL = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    SEARCH_DATE = '2024-01-19 10:45'
+    
 
+    response = requests.get(url=URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find('table')
+
+    
+    def preparing_url(download_url: list):
+        rows = table.find_all('tr')
+        for row in rows:
+            cols = row.find_all(['td', 'th'])
+            if SEARCH_DATE in [ele.text.strip() for ele in cols]:    
+                url_prep = f"{URL}" + row.find('a').get('href')
+                download_url.append(url_prep)
+        
+    
+    download_url = []
+    preparing_url(download_url)
+    print(download_url)
 
 if __name__ == "__main__":
     main()
