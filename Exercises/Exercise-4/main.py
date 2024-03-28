26dbb766f7d9d31a6d6cd4bace75381f96e07101
DenysBosiak <b.denis92@gmail.com>
2024-03-28 17:32:35 +0200

    Update Exercise-4/main.py

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,5 +1,6 @@
 import pathlib
 import json
+import os
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
 
 
     def read_json(file: str) -> dict:
+        # Open json file for reading
         try:
             with open(file, "r") as f:
                 data = json.loads(f.read())
@@ -17,28 +19,56 @@ def main():
         return data
     
 
-    def normalize_json(data: dict) -> dict:
-        normalize_data = dict()
+    def flatten_json(data: dict) -> dict:
+        # Define dictionary for flattening json data
+        flatten_data = dict()
 
+        # Iterate through dictionary
         for key, value in data.items():
+            # Check out if value is not dictionary
             if not isinstance(value, dict):
-                normalize_data[key] = value
+                flatten_data[key] = value
+            # Else generate key from key & subkey
             else:
                 for k, v in value.items():
-                    normalize_data[key + "_" + k] = v
+                    flatten_data[key + "_" + k] = v
 
-        return normalize_data
+        return flatten_data
+    
+
+    def write_csv(file: str, data: dict) -> bool:
+        file_name, file_extension = os.path.splitext(file)
+        csv_file_name = f"{file_name}.csv"
+
+        # Define csv columns in a list
+        csv_columns = data.keys()
+        # Generate the first row of csv
+        csv_data = ",".join(csv_columns) + "\n"
+        print(csv_data)
+ 
+
+        csv_rows = list()
+        for col in csv_columns:
+            csv_rows.append(str(data[col]))
+        
+        csv_data += ",".join(csv_rows) + "\n"       
+
+        try:
+            with open(csv_file_name, "w+") as f:
+                f.write(csv_data)
+        except:
+            raise Exception(f"Saving data to [{csv_file_name}] encountered an error.")
+        
+        print(csv_data)
 
 
+    #
     for file in json_files:
         json_data = read_json(file=file)
-        normalize_json_file = normalize_json(data=json_data)
+        flatten_json_file = flatten_json(data=json_data)
+        write_csv(file=file, data=flatten_json_file)
         print(f"File = [{file}]")
-        print(json_data)
-        print(normalize_json_file)
-        print('')
-
-
+  
 
 
 if __name__ == "__main__":
